cb677f604bdc3c712265f25d3bbfdfba7e8d3173
magnusculley <magnus@udel.edu>
2024-05-07 18:36:23 -0400

    a

--- player.gd
+++ player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 
@@ -17,3 +16,33 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
+	if velocity.y!=0 || velocity.x!=0:
+		animate(velocity)
+	else:
+		sprite.animation="default"
+
+
+func animate(v):
+	if v==Vector2(SPEED,SPEED):
+		sprite.play("dright")
+	elif v==Vector2(-SPEED,SPEED):
+		sprite.play("dleft")
+	elif v==Vector2(SPEED,-SPEED):
+		sprite.play("uright")
+	elif v==Vector2(-SPEED,-SPEED):
+		sprite.play("uleft")
+	elif v==Vector2(0,SPEED):
+		sprite.play("down")
+	elif v==Vector2(0,-SPEED):
+		sprite.play("up")
+	elif v==Vector2(SPEED,0):
+		sprite.play("right")
+	elif v==Vector2(-SPEED,0):
+		sprite.play("left")
+	
+	#if Input.is_action_pressed("up"):
+		#if self.scale>Vector2(.8,.8):
+			#self.scale-=Vector2(.005,.005)
+	#elif Input.is_action_pressed("down"):
+		#if self.scale<Vector2(1.2,1.2):
+			#self.scale+=Vector2(.005,.005)
